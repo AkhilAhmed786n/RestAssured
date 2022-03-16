ed39e74f494eb98f47196fe5d1e28be54abd0e63
AkhilAhmed786n <akhilahmed786n@gmail.com>
2022-03-16 16:07:19 +0530

    commit 3

--- src/test/java/responseTypes/DynamicResponse.java
+++ src/test/java/responseTypes/DynamicResponse.java
@@ -13,16 +13,19 @@ public class DynamicResponse {
 	@Test
 	public void dynamicResponse()
 	{
-		String expData="Pokemon";
+		String expData="pojoProj";
 		Response response = when().get("http://localhost:8084/projects");
 		response.then().assertThat().statusCode(200);
+		String con = response.getHeader("Connection");
+		
+		System.out.println(con);
 		
 		List<String> listData = response.jsonPath().get("projectName");
-		
+		System.out.println(response.getDetailedCookie("JSESSIONID"));
 		
 		for(String actData:listData)
 		{
-			System.out.println(actData);
+//			System.out.println(actData);
 			if(actData.equals(expData))
 			{
 				System.out.println(actData+" is Present.");
